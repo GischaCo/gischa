15b45d3b94894595a1c9981a53ff40a81a7099cf
Donya <thisis2nya@gmail.com>
2023-09-11 21:12:41 +0330

    core: config website info

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,15 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "Gischa | گیشا",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || "",
+        content:
+          "قالب رایگان آکادمی گیشا دورهمی برنامه نویسان پادکست برنامه نویسی",
       },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
