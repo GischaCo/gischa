57b8b9b52bcc1bef0dc3f50c016e4587bf57a6e8
Donya <thisis2nya@gmail.com>
2023-09-11 20:18:57 +0330

    core: install & setup tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,17 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        gischa
-      </h1>
-      <h2 class="subtitle">
-        gischa app
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div
+    class="w-screen h-screen flex flex-col items-center justify-center gap-12"
+  >
+    <logo></logo>
+    <h1 class="text-purple-800 text-5xl font-bold font-serif">Gischa App</h1>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import logo from "../components/Logo.vue";
 export default {
   components: {
-    Logo
-  }
-}
+    logo,
+  },
+};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
