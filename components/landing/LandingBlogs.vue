5b69c45dd0574a8454749fbe282e93e37c4ba129
Donya <thisis2nya@gmail.com>
2023-09-16 17:29:26 +0330

    UI: re-design landing's recent blogs section

--- components/landing/LandingBlogs.vue
+++ components/landing/LandingBlogs.vue
@@ -3,30 +3,12 @@
     <h2 class="text-2xl font-bold text-primary text-center">
       آخرین مقالات بلاگ
     </h2>
-    <div class="w-full flex items-center justify-center gap-4 py-8">
-      <nuxt-link to="/" v-for="(blog, i) in recentBlogs" :key="i">
-        <div
-          class="w-72 h-40 p-4 flex flex-col items-start justify-between gap-3 rounded-xl border-e-4 border-s border-primary"
-        >
-          <!-- title -->
-          <h3 class="text-lg font-bold text-primary">{{ blog.title }}</h3>
-
-          <!-- details -->
-          <div class="w-full flex items-center justify-between">
-            <!-- author -->
-            <div class="flex items-center justify-start gap-1">
-              <base-icon
-                name="pen-solid"
-                class="w-3 fill-secondary"
-              ></base-icon>
-              <p class="text-sm text-secondary">{{ blog.author }}</p>
-            </div>
-
-            <!-- date -->
-            <p class="text-sm text-secondary">{{ blog.date }}</p>
-          </div>
-        </div>
-      </nuxt-link>
+    <div class="w-full flex items-center justify-center gap-6 py-8">
+      <landing-blogs-item
+        v-for="(blog, i) in recentBlogs"
+        :key="i"
+        :blog="blog"
+      ></landing-blogs-item>
     </div>
 
     <!-- see more -->
@@ -46,18 +28,30 @@ import { ref } from "@nuxtjs/composition-api";
 // variables
 const recentBlogs = ref([
   {
+    path: "/blog/e98ew0f98sixko",
     title: "مهندسی نرم افزار یا Software Engineering چیست؟",
-    author: "دنیا داوودی",
+    author: {
+      name: "دنیا داوودی",
+      avatar: require("@/assets/img/manager-profile.jpg"),
+    },
     date: "25 شهریور 1402",
   },
   {
+    path: "/blog/cd0f9sdf8sosxf",
     title: "لاراول 10 و مهم‌ترین ویژگی‌های آن",
-    author: "دنیا داوودی",
+    author: {
+      name: "دنیا داوودی",
+      avatar: require("@/assets/img/manager-profile.jpg"),
+    },
     date: "25 شهریور 1402",
   },
   {
+    path: "/blog/6sd56fcsdvfwer",
     title: "Socket.io چیست و چه کاربردی دارد؟ + پروژه عملی",
-    author: "دنیا داوودی",
+    author: {
+      name: "دنیا داوودی",
+      avatar: require("@/assets/img/manager-profile.jpg"),
+    },
     date: "25 شهریور 1402",
   },
 ]);
---
+++ components/landing/LandingBlogsItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <nuxt-link :to="blog.path">
+    <div
+      class="relative w-72 h-40 p-4 bg-light flex flex-col items-start justify-between gap-3 rounded-xl rounded-br-3xl shadow-lg shadow-dark/10 hover:shadow-xl hover:-translate-y-1 transition-all duration-300"
+    >
+      <!-- title -->
+      <h3 class="text-lg text-primary font-bold">{{ blog.title }}</h3>
+      <!-- details -->
+      <div
+        class="absolute bottom-0 left-0 w-full h-12 p-1 bg-white flex items-center justify-between shadow-sm rounded-l rounded-r-full"
+      >
+        <!-- author -->
+        <div class="h-full flex items-center justify-start gap-2">
+          <!-- avatar -->
+          <img
+            class="h-full rounded-full"
+            :src="blog.author.avatar"
+            :alt="`${blog.author.name} - ${blog.title}`"
+          />
+          <p class="text-sm text-secondary">{{ blog.author.name }}</p>
+        </div>
+
+        <!-- date -->
+        <p class="text-sm text-secondary">{{ blog.date }}</p>
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  nmae: "LandingBlogsItem",
+};
+</script>
+
+<script setup>
+defineProps({
+  blog: {
+    type: Object,
+    required: true,
+    default: function () {
+      return {};
+    },
+  },
+});
+</script>
